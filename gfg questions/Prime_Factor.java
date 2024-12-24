d0a83552098153e2fcdb6709b2eebfffe56a7690
Vanshika <118618760+Vanshikaduggal@users.noreply.github.com>
2024-12-24 12:10:23 +0530

    Update Prime_Factor.java

--- gfg questions/Prime_Factor.java
+++ gfg questions/Prime_Factor.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int[] AllPrimeFactors(int N) {
+        // Create a list to store unique prime factors
+        ArrayList<Integer> primeFactors = new ArrayList<>();
+        for (int i = 2; i <= N; i++) {
+            if (N % i == 0) {
+                primeFactors.add(i);
+                while (N % i == 0) {
+                    N /= i;
+                }
+            }
+        }
+        
+        int[] result = new int[primeFactors.size()];
+        for (int j = 0; j < primeFactors.size(); j++) {
+            result[j] = primeFactors.get(j);
+        }
+        return result;
+    }
+}
 
