86878e5234c96908582cd0721820a76d5e5644fe
Vanshika <118618760+Vanshikaduggal@users.noreply.github.com>
2025-03-27 12:30:05 +0530

    Create Implement atoi and itoa functions.java

---
+++ Testpad/Implement atoi and itoa functions.java
@@ -0,0 +1,13 @@
+class Result {
+  static String itoa(int num) {
+    // Write your code here
+      String str = String.valueOf(num);
+      return str;
+  }
+
+  static int atoi(String str) {
+    // Write your code here
+      int num = Integer.parseInt(str);
+      return num;
+  }
+}
